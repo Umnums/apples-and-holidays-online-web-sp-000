e985b42aa3a600345bc7e77d92ff089082fbf8be
Joseph Kao <josephtkao@gmail.com>
2020-04-13 17:26:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,8 +106,7 @@ def all_holidays_with_bbq(holiday_hash)
 
     end
   end
-  unused
-end
-end
+
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
