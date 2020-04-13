9d156e4553d11183e7201340c65760c722054329
Joseph Kao <josephtkao@gmail.com>
2020-04-13 16:30:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}: "
     holiday.each do |holiday, values|
-      puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{values.to_s}"
+      puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{values.join(", ")}"
 
       end
     end
